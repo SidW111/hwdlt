394562653520508c659106e455d53672491edd53
SidW111 <siddhantwainngade@gmail.com>
2025-07-31 20:51:44 +0530

    added delete note function

--- backend/src/controllers/note.controller.ts
+++ backend/src/controllers/note.controller.ts
@@ -15,23 +15,35 @@ export const createNote = async (req: AuthRequest, res: Response) => {
       });
     }
   } catch (error) {
-    console.log("Error Creating note",error);
+    console.log("Error Creating note", error);
     res.json(500).json({
-        message:"error creating note",
-    })
+      message: "error creating note",
+    });
   }
 };
 
-export const getNotes = async(req:AuthRequest,res:Response) => {
+export const getNotes = async (req: AuthRequest, res: Response) => {
+  try {
+    const note = await NoteModel.findOne({ user: req.user.id });
 
-    try {
-        const note = await NoteModel.findOne({user:req.user.id});
+    res.json(note);
+  } catch (error) {
+    console.log("error finding notes");
+    res.status(500).json({ message: "error finding notes" });
+  }
+};
 
-        res.json(note)
-    } catch (error) {
-        console.log("error finding notes");
-        res.status(500).json({message:"error finding notes"})
-    }
-
-}
-
+export const deleteNote = async (req: AuthRequest, res: Response) => {
+  const noteId = req.params.id;
+  try {
+    const note = await NoteModel.findOneAndDelete({
+      _id: noteId,
+      user: req.user.id,
+    });
+    if (!note) return res.status(404).json({ error: "Note not found" });
+    res.json({ message: "Note deleted" });
+  } catch (error) {
+    console.log("error deleting node", error);
+    res.status(500).json({ error: "Failed to delete note" });
+  }
+};
