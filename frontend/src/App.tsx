3403d903a5f0aada3616c74dc452e08594798142
SidW111 <siddhantwainngade@gmail.com>
2025-07-31 13:55:56 +0530

    initialised frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="text-6xl hover:bg-black hover:text-white transition-all duration-1000">
+      hii
+    </div>
+  );
+};
+
+export default App;
